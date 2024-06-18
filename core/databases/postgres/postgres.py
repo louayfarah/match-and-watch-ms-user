a89d3a46fd473309013dfdee50291319656d64e6
Louay Farah <l.farah@innopolis.university>
2024-06-18 23:05:11 +0300

    Refactor database connection setup in postgres.py

--- core/databases/postgres/postgres.py
+++ core/databases/postgres/postgres.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from util.const import SQLALCHEMY_DATABASE_URI
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 
 def get_db():
