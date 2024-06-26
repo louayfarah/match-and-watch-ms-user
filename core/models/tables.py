47c23136566315023c9144c48e5a49d3318fad30
saleemasekrea000 <s.asekrea@innopolis.university>
2024-06-26 07:37:05 +0300

    dded endpoints for managing user preferences

--- core/models/tables.py
+++ core/models/tables.py
@@ -1,7 +1,7 @@
 import uuid
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy import Column, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, ARRAY, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -14,3 +14,21 @@ class User(Base):
     email = Column(String(255), unique=True, index=True)
     password = Column(String(255))
     salt = Column(String(255))
+    preferences = relationship(
+        "UserPreferences", backref="user", cascade="all,delete-orphan"
+    )
+
+
+class UserPreferences(Base):
+    __tablename__ = "user_preferences"
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = Column(
+        UUID(as_uuid=True), ForeignKey("users.id"), index=True)
+    genres = Column(ARRAY(String))
+    favorite_type = Column(String)
+    watching_habits = Column(String)
+    preferred_language = Column(String)
+    age_preference = Column(String)
+    favorite_directors_actors = Column(ARRAY(String))
+    preferred_length = Column(String)
+    review_sources = Column(ARRAY(String))
--- routers/users.py
+++ routers/users.py
@@ -1,9 +1,10 @@
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from sqlalchemy.orm import Session
 from dependencies import get_db
 from util import users
 from core.schemas import schemas
-
+from dependencies import dependencies
+from core.crud import crud
 
 router = APIRouter()
 
@@ -16,3 +17,20 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 @router.post("/api/login", tags=["Authentication"])
 def login(request: schemas.LoginRequest, db: Session = Depends(get_db)):
     return users.login(request, db)
+
+
+@router.post("/api/user/preferences/", response_model=schemas.UserPreferences)
+def create_user_preferences(
+    preferences: schemas.UserPreferencesCreate,
+    user: schemas.AuthenticatedUser = Depends(dependencies.get_user),
+    db: Session = Depends(get_db)
+):
+    return users.create_user_preferences(preferences, user, db)
+
+
+@router.get("/api/user/preferences/", response_model=schemas.UserPreferences)
+def get_user_preferences(
+    db: Session = Depends(get_db),
+    user: schemas.AuthenticatedUser = Depends(dependencies.get_user)
+):
+    return users.get_user_preferences(user, db)
